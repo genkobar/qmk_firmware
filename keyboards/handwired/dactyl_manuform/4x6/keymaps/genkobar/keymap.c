962b0f01ceb13c69fdf94574c55f612a796882a1
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2021-07-18 11:50:15 +0000

    Move ] and - for Icelandic

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/genkobar/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/genkobar/keymap.c
@@ -36,13 +36,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base (qwerty)
  * +-----------------------------------------+                             +-----------------------------------------+
- * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |   -  |
+ * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |   [  |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
  * | TAB  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |   '  |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |   \  |
+ * | LSHFT|   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |   \  |
  * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- * | ICEL |      |  [   |   ]  |                                                         |   +  |   =  |      |      |
+ * | OS   | LALT |  [   |   ]  |                                                         |   +  |   =  |   -  | RSHFT|
  * +---------------------------+--------------------+               +--------------------+---------------------------+
  *                             |      |      |      |               |      |      |      |
  *                             +------+      |      |               |      |      +------+
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-    KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
+    KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   \
     KC_LCTL, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
     KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
-    OSFUNC,KC_LALT,KC_LBRC,KC_RBRC,                                             KC_PLUS,KC_EQL,KC_RALT,KC_RSFT,  \
+    OSFUNC,KC_LALT,KC_LBRC,KC_RBRC,                                             KC_SPC,KC_EQL,KC_MINS,KC_RSFT,  \
                                     SYMBOLS,KC_BSPC,            KC_DEL, NUMPAD,                                     \
                                     KC_TAB,  KC_SPC,            KC_ENT, KC_LCTL,                                    \
                                     HYPER, KC_GRV,              KC_LALT, KC_LGUI                                   \
