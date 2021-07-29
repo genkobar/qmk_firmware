246e8b10dd34d15c61b7a4476839841fa8dd611e
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2021-07-29 14:48:23 +0000

    Move backspace back to thumb cluster

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/genkobar/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/genkobar/keymap.c
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   \
     LCTL_ESC, GUI_A,  ALT_S,   SFT_D,   CTK_F,  KC_G,               KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_QUOT,   \
     KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
-    OSFUNC,KC_LALT,KC_LBRC,KC_RBRC,                                             KC_RALT,KC_EQL,KC_MINS,KC_BSPC,  \
+    OSFUNC,HYPER,KC_LBRC,KC_RBRC,                                             KC_RALT,KC_EQL,KC_MINS,KC_BSPC,  \
                                     NUMPAD, SYMBOLS,            KC_TAB, KC_GRV,                                     \
                                     KC_LALT,  KC_LGUI,          KC_RGUI, KC_RALT,                                    \
-                                    KC_ENT, HYPER,              RCTL_ESC, KC_SPC                                   \
+                                    KC_ENT, KC_BSPC,              RCTL_ESC, KC_SPC                                   \
 ),
 
 [_HALMAK] = LAYOUT( \
