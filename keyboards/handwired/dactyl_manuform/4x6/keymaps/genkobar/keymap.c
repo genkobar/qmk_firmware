f9812af33854dd2453cf29cc175f27442b8811d1
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2021-08-07 02:40:02 +0000

    Tweak thumb cluster

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/genkobar/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/genkobar/keymap.c
@@ -76,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LCTL_ESC, KC_A,  KC_S,   KC_D,   KC_F,  KC_G,               KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_QUOT,   \
     KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
     OSFUNC,HYPER,KC_LBRC,KC_RBRC,                                             KC_RALT,KC_EQL,KC_MINS,KC_BSPC,  \
-                            SYMBOLS, KC_BSPC, KC_LGUI,      KC_RGUI, KC_SPC,  KC_TAB,                           \
-                            NUMPAD, KC_ENT, KC_LALT,        KC_RALT, RCTL_ESC, KC_GRV                           \
+                            KC_LGUI, KC_BSPC, KC_ENT,       KC_TAB, KC_SPC, RCTL_ESC,                           \
+                            KC_LALT, LCTL_ESC, NUMPAD,      KC_GRV, KC_RALT, KC_RSFT                    \
 ),
 
 [_HALMAK] = LAYOUT( \
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUMPAD] = LAYOUT(
     _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_PLUS,   \
     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_PIPE,   \
-    _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_UNDS,   \
+    SYMBOLS,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_UNDS,   \
     _______,_______,_______,KC_PSCR,                                            KC_P0,_______,_______,_______,    \
                                     _______,_______,_______,    _______,_______,_______,                           \
-                                    _______,_______,_______,    _______,_______,_______                           \
+                                    _______,_______,_______,    _______,_______,SYMBOLS\
 ),
 
 /* Symbols
