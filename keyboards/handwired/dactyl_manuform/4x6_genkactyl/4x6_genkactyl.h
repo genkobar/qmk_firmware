06b6752b1cb25bc45590e6c2e4b67b012bd6ea32
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2021-10-16 22:33:11 +0000

    Reorganize layers and move numsym key

--- keyboards/handwired/dactyl_manuform/4x6_genkactyl/4x6_genkactyl.h
+++ keyboards/handwired/dactyl_manuform/4x6_genkactyl/4x6_genkactyl.h
@@ -20,13 +20,14 @@
 #include "quantum.h"
 
 #ifdef USE_I2C
-#include <stddef.h>
-#ifdef __AVR__
-	#include <avr/io.h>
-	#include <avr/interrupt.h>
-#endif
+#    include <stddef.h>
+#    ifdef __AVR__
+#        include <avr/io.h>
+#        include <avr/interrupt.h>
+#    endif
 #endif
 
+// clang-format off
 #define LAYOUT(\
     L00, L01, L02, L03, L04, L05,                    R00, R01, R02, R03, R04, R05, \
     L10, L11, L12, L13, L14, L15,                    R10, R11, R12, R13, R14, R15, \
@@ -49,3 +50,4 @@
         { R40, R41, R42, R43, KC_NO, KC_NO } \
 \
    }
+// clang-format on
